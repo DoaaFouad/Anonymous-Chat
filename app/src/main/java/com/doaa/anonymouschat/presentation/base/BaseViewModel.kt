3c37ee7e3dbb9c84007ca248159f06ebf2554f8d
Doaa Fouad <doaa_fouad2006@hotmail.com>
2022-01-13 22:27:39 +0400

    Add project dependencies

---
+++ app/src/main/java/com/doaa/anonymouschat/presentation/base/BaseViewModel.kt
@@ -0,0 +1,15 @@
+/*
+ * *
+ * Created by Doaa Fouad on 1/13/22 10:21 PM
+ * doaa_fouad2006@hotmail.com
+ * github.com/DoaaFouad
+ * linkedin.com/in/doaafouad/
+ * Copyright (c) 2022 . All rights reserved.
+ * Last modified 1/13/22 10:21 PM
+ *
+ */
+
+package com.doaa.anonymouschat.presentation.base
+
+class BaseViewModel {
+}
---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,60 @@
+/*
+ *
+ * Created by Doaa Fouad on 1/13/22 9:58 PM
+ * doaa_fouad2006@hotmail.com
+ * github.com/DoaaFouad
+ * linkedin.com/in/doaafouad/
+ * Copyright (c) 2022 . All rights reserved.
+ * Last modified 1/13/22 9:53 PM
+ *
+ */
+
+object BuildConfigs {
+    const val COMPILE_SDK = 30
+    const val MIN_SDK = 24
+    const val TARGET_SDK = 30
+
+    const val DEVELOPMENT_VERSION_CODE = 1
+    const val DEVELOPMENT_VERSION_NAME = "1.0"
+}
+
+object Versions {
+    // Kotlin
+    const val KOTLIN = "1.3.72"
+
+    // UI
+    const val ANDROID_X = "1.3.2"
+    const val APPCOMPAT = "1.2.0"
+    const val MATERIAL = "1.1.0"
+    const val GLIDE = "4.11.0"
+
+    // DI
+    const val KOIN = "2.0.1"
+    const val KOIN_ANDROID = "2.0.1"
+
+    //Coroutines
+    const val COROUTINES_CORE = "1.4.3"
+    const val COROUTINES_ANDROID = "1.4.0"
+}
+
+object Libraries {
+    // Kotlin
+    const val KOTLIN = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.KOTLIN}"
+
+    // UI
+    const val ANDROID_X = "androidx.core:core-ktx:${Versions.ANDROID_X}"
+    const val APPCOMPAT = "androidx.appcompat:appcompat:${Versions.APPCOMPAT}"
+    const val MATERIAL = "com.google.android.material:material:${Versions.MATERIAL}"
+    const val GLIDE = "com.github.bumptech.glide:glide:${Versions.GLIDE}"
+
+    // DI
+    const val KOIN = "org.koin:koin-core:${Versions.KOIN}"
+    const val KOIN_ANDROID = "org.koin:koin-androidx-viewmodel:${Versions.KOIN_ANDROID}"
+
+    // Coroutines
+    const val COROUTINES_CORE = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.COROUTINES_CORE}"
+    const val COROUTINES_ANDROID = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.COROUTINES_ANDROID}"
+}
+
+
+
